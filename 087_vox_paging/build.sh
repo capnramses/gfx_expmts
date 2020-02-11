931c5f586d61120169bfe086103030507ab57079
Anton Gerdelan <antonofnote@gmail.com>
2020-02-11 15:36:59 +0000

    ubsan added

--- 087_vox_paging/build.sh
+++ 087_vox_paging/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-clang -fsanitize=address -Wall -Wextra -Wfatal-errors -pedantic -g \
+clang -fsanitize=address -fsanitize=undefined -Wall -Wextra -Wfatal-errors -pedantic -g \
 main.c voxels.c apg_ply.c apg_pixfont.c camera.c input.c gl_utils.c diamond_square.c \
 ../common/src/GL/glew.c -I../common/include/ -I ../common/include/stb/ -lm -lglfw -lGL
