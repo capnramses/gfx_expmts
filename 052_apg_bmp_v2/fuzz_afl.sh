d624b23486321138012efc6f083bd156db1ec90f
Anton Gerdelan <antonofnote@gmail.com>
2020-03-05 14:53:50 +0000

    fixed issue in fuzz script

--- 052_apg_bmp_v2/fuzz_afl.sh
+++ 052_apg_bmp_v2/fuzz_afl.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+BIN=fuzz_readwritetest
+INDIR=fuzz_test_inputs
+OUTDIR=fuzz_test_outputs
+
+#set up folders
+rm -f $BIN
+rm -rf $OUTDIR
+mkdir $OUTDIR
+
 #build for fuzzing
-afl-gcc -g -o fuzz_readwritetest main_test.c apg_bmp.c
-mkdir fuzz_test_outputs/
+afl-gcc -g -o $BIN main_test.c apg_bmp.c
+
 #run with fuzz
 AFL_EXIT_WHEN_DONE=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
-AFL_SKIP_CPUFREQ=1 afl-fuzz -i fuzz_test_inputs/ -o fuzz_test_outputs/ -- ./fuzz_readwritetest @@ current_out.bmp	
+AFL_SKIP_CPUFREQ=1 afl-fuzz -i $INDIR/ -o $OUTDIR/ -- ./$BIN @@ $OUTDIR/current_out.bmp	
