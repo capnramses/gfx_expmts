543fc95f513cbeb2feaf8420729ff1bcf33a34ee
Anton Gerdelan <antonofnote@gmail.com>
2020-03-05 14:29:57 +0000

    added script for fuzzing apg_bmp

---
+++ 052_apg_bmp_v2/fuzz_afl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#build for fuzzing
+afl-gcc -g -o fuzz_readwritetest main_test.c apg_bmp.c
+mkdir fuzz_test_outputs/
+#run with fuzz
+AFL_EXIT_WHEN_DONE=1 AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
+AFL_SKIP_CPUFREQ=1 afl-fuzz -i fuzz_test_inputs/ -o fuzz_test_outputs/ -- ./fuzz_readwritetest @@
--- 052_apg_bmp_v2/test_run.sh
+++ 052_apg_bmp_v2/test_run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
-cd fuzz_test_inputs/
+mkdir test_outputs
+cd test_inputs/
 for filename in *.*; do
-	../readwritetest $filename ../test_output/$filename
+	../readwritetest $filename ../test_outputs/$filename
 	#if [[ $ret != 0 ]]; then exit $ret; fi
 done
 cd ..
