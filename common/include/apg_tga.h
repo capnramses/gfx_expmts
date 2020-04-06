d6f267282c20b840e401f2ce9e0d172949861121
Anton Gerdelan <antonofnote@gmail.com>
2020-04-06 19:39:49 +0100

    updated apg_tga

--- common/include/apg_tga.h
+++ common/include/apg_tga.h
@@ -1,6 +1,6 @@
 /*==============================================================
 Single-Header TGA image file reader/writer
-Language: C89
+Language: C89 header, C99 implementation.
 Author:   Anton Gerdelan - @capnramses
 Contact:  <antonofnote@gmail.com>
 Website:  https://github.com/capnramses/apg - antongerdelan.net/
@@ -10,27 +10,29 @@ Instructions:
 1. Include this header in one, and only one, source file.
 2. #define APG_TGA_IMPLEMENTATION above #include "apg_tga.h"
 3.
-int w,h,n;
+unsigned int w,h,n;
 uint8_t* img_ptr = apg_tga_read_file("my_file.tga", &w, &h, &n, 0);
 
 // ... use the BGR or BGRA (when n == 4) image memory here ...
 
 free( img_ptr );
 
+Define APG_TGA_DEBUG_OUTPUT to get extra information printed to stdout.
+
 Limitations:
 * Only reads/writes true colour uncompressed BGR and BGRA images.
-* Note - vertical flip 0/1 might be round the wrong way after testing in D3D.
+* Note - There are inconsistent vertical flip conventions between users of TGA. We do our best here.
 
 Todo:
 * fuzzing
+* validate range of dims etc - uint16_t internally so check range
 * could allow malloc/free override
-* could add a separate function or parameter to convert RGB<->BGR
 
 History:
-09/09/2019 - first version.
-24/09/2019 - published to apg repository.
-14/11/2019 - fixes for MSVC warnings (CPP compat)
-06/04/2020 - minor tidy-up of versions between repos
+09/09/2019 - First version.
+24/09/2019 - Published to apg repository.
+14/11/2019 - Fixes for MSVC warnings (CPP compat)
+06/04/2020 - Tidy-up between repos. Added BGR<->RGB utility function. Bugfix: Writing. Y direction for GIMP etc. APG_TGA_DEBUG_OUTPUT option.
 ==============================================================*/
 
 #ifndef APG_TGA_H
@@ -40,10 +42,14 @@ History:
 extern "C" {
 #endif
 
-/* returns a pointer to tightly-packed 8-bpp BGR memory, or NULL on error or unsupported TGA subtype. */
-unsigned char* apg_tga_read_file( const char* filename, int* w, int* h, int* n, int vert_flip );
-/* returns 1 on success, 0 on error */
-unsigned int apg_tga_write_file( const char* filename, unsigned char* bgr_img_ptr, int w, int h, int n );
+/* RETURNS A pointer to tightly-packed 8-bpp BGR or BGRA memory, or NULL on error or unsupported TGA subtype. */
+unsigned char* apg_tga_read_file( const char* filename, unsigned int* w, unsigned int* h, unsigned int* n, unsigned int vert_flip );
+/* RETURNS 1 on success, 0 on error. */
+unsigned int apg_tga_write_file( const char* filename, unsigned char* bgr_img_ptr, unsigned int w, unsigned int h, unsigned int n );
+
+/* Flips BGR[A] to RGB[A] or vice versa
+RETURNS 1 on success, 0 on error. */
+unsigned int apg_tga_bgr_to_rgb( unsigned char* img_ptr, unsigned int w, unsigned int h, unsigned int n );
 
 #ifdef __cplusplus
 }
@@ -55,6 +61,7 @@ End of Header
 ==============================================================*/
 
 #ifdef APG_TGA_IMPLEMENTATION
+#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -74,7 +81,26 @@ struct tga_header_t {
   uint16_t x_origin, y_origin; /* absolute coordinate of lower-left corner for displays where origin is at the lower left */
   uint16_t w, h;               /* img dims in pixels */
   uint8_t bpp;
-  uint8_t img_descriptor; /* bits 3-0 give the alpha channel depth, bits 5-8 give direction */
+  uint8_t img_descriptor;
+  /* bits 3-0 give the alpha channel depth, bits 5-8 give direction
+
+from http://www.gamers.org/dEngine/quake3/TGA.txt:
+|   17   |     1  |  Image Descriptor Byte.                                    |
+|        |        |  Bits 3-0 - number of attribute bits associated with each  |
+|        |        |             pixel.                                         |
+|        |        |  Bit 4    - reserved.  Must be set to 0.                   |
+|        |        |  Bit 5    - screen origin bit.                             | <-- important for eg GIMP
+|        |        |             0 = Origin in lower left-hand corner.          |
+|        |        |             1 = Origin in upper left-hand corner.          |
+|        |        |             Must be 0 for Truevision images.               |
+|        |        |  Bits 7-6 - Data storage interleaving flag.                |
+|        |        |             00 = non-interleaved.                          |
+|        |        |             01 = two-way (even/odd) interleaving.          |
+|        |        |             10 = four way interleaving.                    |
+|        |        |             11 = reserved.                                 |
+|        |        |  This entire byte should be set to 0.  Don't ask me.       |
+
+  */
 };
 #pragma pack( pop )
 
@@ -83,11 +109,11 @@ struct file_record_t {
   size_t sz; /* in bytes */
 };
 
-unsigned char* apg_tga_read_file( const char* filename, int* w, int* h, int* n, int vert_flip ) {
+unsigned char* apg_tga_read_file( const char* filename, unsigned int* w, unsigned int* h, unsigned int* n, unsigned int vert_flip ) {
   struct file_record_t record;
   uint8_t* img_ptr     = NULL;
   size_t img_id_offset = 0, colour_map_offset = 0, img_data_offset = 0, img_data_sz = 0;
-  int vflip = 0;
+  bool vflip = 0;
 
   if ( !filename || !w || !h || !n ) { return NULL; }
   {
@@ -112,7 +138,22 @@ unsigned char* apg_tga_read_file( const char* filename, int* w, int* h, int* n, 
       free( record.data );
       return NULL;
     }
-    hdr_ptr           = (struct tga_header_t*)record.data;
+    hdr_ptr = (struct tga_header_t*)record.data;
+#ifdef APG_TGA_DEBUG_OUTPUT
+    printf( "TGA hdr for `%s`\n", filename );
+    printf( " |-id_length: %u\n", hdr_ptr->id_length );
+    printf( " |-colour_map_type: %u\n", hdr_ptr->colour_map_type );
+    printf( " |-image_type: %u\n", hdr_ptr->image_type );
+    printf( " |-colour_map_first_entry_idx: %u\n", hdr_ptr->colour_map_first_entry_idx );
+    printf( " |-colour_map_length: %u\n", hdr_ptr->colour_map_length );
+    printf( " |-colour_map_bpp: %u\n", hdr_ptr->colour_map_bpp );
+    printf( " |-x_origin: %u\n", hdr_ptr->x_origin );
+    printf( " |-y_origin: %u\n", hdr_ptr->y_origin );
+    printf( " |-w: %u\n", hdr_ptr->w );
+    printf( " |-h: %u\n", hdr_ptr->h );
+    printf( " |-bpp: %u\n", hdr_ptr->bpp );
+    printf( " |-img_descriptor: %u\n", hdr_ptr->img_descriptor );
+#endif
     *w                = hdr_ptr->w;
     *h                = hdr_ptr->h;
     *n                = 32 == hdr_ptr->bpp ? 4 : 3;
@@ -139,7 +180,7 @@ unsigned char* apg_tga_read_file( const char* filename, int* w, int* h, int* n, 
       return NULL;
     }
     /* vertical flip so 0,0 is bottom-left */
-    //if ( 0 == hdr_ptr->y_origin ) { vflip = 1; }
+    if ( 0 == hdr_ptr->y_origin ) { vflip = 1; }
   }
   {
     img_ptr = (uint8_t*)malloc( img_data_sz );
@@ -148,11 +189,10 @@ unsigned char* apg_tga_read_file( const char* filename, int* w, int* h, int* n, 
       return NULL;
     }
   }
-  if ( vert_flip ) { vflip = 1; }
+  if ( vert_flip ) { vflip = true; }
   if ( vflip ) {
     size_t row_stride = *w * *n;
-    int row;
-    for ( row = 0; row < *h; row++ ) {
+    for ( unsigned int row = 0; row < *h; row++ ) {
       uint8_t* src_ptr = record.data + img_data_offset + row * row_stride;
       uint8_t* dst_ptr = img_ptr + ( *h - row - 1 ) * row_stride;
       memcpy( dst_ptr, src_ptr, row_stride );
@@ -164,18 +204,20 @@ unsigned char* apg_tga_read_file( const char* filename, int* w, int* h, int* n, 
   return img_ptr;
 }
 
-unsigned int apg_tga_write_file( const char* filename, unsigned char* bgr_img_ptr, int w, int h, int n ) {
+unsigned int apg_tga_write_file( const char* filename, unsigned char* bgr_img_ptr, unsigned int w, unsigned int h, unsigned int n ) {
   struct tga_header_t hdr;
 
   if ( !filename || !bgr_img_ptr ) { return 0; }
 
   {
     memset( &hdr, 0, sizeof( struct tga_header_t ) );
-    hdr.image_type = 2;
-    hdr.w          = (uint16_t)w;
-    hdr.h          = (uint16_t)h;
-    hdr.y_origin   = (uint16_t)h;
-    hdr.bpp        = (uint8_t)(8 * n);
+    hdr.image_type     = 2;
+    hdr.w              = (uint16_t)w;
+    hdr.h              = (uint16_t)h;
+    hdr.y_origin       = (uint16_t)h;
+    hdr.bpp            = ( uint8_t )( 8 * n );
+    hdr.img_descriptor = 40; /* NOTE(Anton) if wrong, eg set to zero, then image may be upside-down.
+    bits 3-0 give the alpha channel depth, bits 5-8 give direction */
   }
   {
     size_t nw     = 0;
@@ -194,6 +236,20 @@ unsigned int apg_tga_write_file( const char* filename, unsigned char* bgr_img_pt
     }
     fclose( fptr );
   };
+  return 1;
+}
+
+unsigned int apg_tga_bgr_to_rgb( unsigned char* img_ptr, unsigned int w, unsigned int h, unsigned int n ) {
+  if ( !img_ptr || !w || !h || !n ) { return 0; }
+  if ( n != 3 && n != 4 ) { return 0; }
+  for ( unsigned int y = 0; y < h; y++ ) {
+    for ( unsigned int x = 0; x < w; x++ ) {
+      unsigned int idx  = y * w * n + x * n;
+      unsigned char tmp = img_ptr[idx + 0];
+      img_ptr[idx + 0]  = img_ptr[idx + 2];
+      img_ptr[idx + 2]  = tmp;
+    }
+  }
   return 1;
 }
 
