0e54c3ce9e26ef931d35de67329170607cffd00f
Anton Gerdelan <antonofnote@gmail.com>
2020-12-01 17:36:51 +0000

    scripts

---
+++ 101_gltf_parser/run.sh
@@ -0,0 +1,32 @@
+Reading `../../glTF-Sample-Models/2.0/FlightHelmet/glTF/FlightHelmet.gltf`
+gltf_ptr->scenes_ptr[0].n_node_idxs=6
+version 	= 2.0
+n_scenes 	= 1
+n_nodes 	= 6
+scene 		= 0
+scene 0:
+  node 0 	= 0
+  node 1 	= 1
+  node 2 	= 2
+  node 3 	= 3
+  node 4 	= 4
+  node 5 	= 5
+node 0:
+  mesh 	= 0
+  name 	= `Hose_low`
+node 1:
+  mesh 	= 1
+  name 	= `RubberWood_low`
+node 2:
+  mesh 	= 2
+  name 	= `GlassPlastic_low`
+node 3:
+  mesh 	= 3
+  name 	= `MetalParts_low`
+node 4:
+  mesh 	= 4
+  name 	= `LeatherParts_low`
+node 5:
+  mesh 	= 5
+  name 	= `Lenses_low`
+done
