a5800a88f3ed4b0763dacccc713b78451359ccfd
Anton Gerdelan <antonofnote@gmail.com>
2021-05-17 22:01:59 +0100

    shader nearly done

--- 108_subnautica_build/main.c
+++ 108_subnautica_build/main.c
@@ -1,5 +1,6 @@
 #include "gfx.h"
 #include "apg_maths.h"
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -19,8 +20,12 @@ int main( int argc, char** argv ) {
   float scale = 1.0f;
   if ( mesh.bounding_radius > 0.0f ) { scale = 1.0f / mesh.bounding_radius; }
   gfx_shader_t shader = gfx_create_shader_program_from_files( "shader.vert", "shader.frag" ); // TODO create shader
+  gfx_texture_t simplex =
+    gfx_texture_create_from_file( "simplex.png", ( gfx_texture_properties_t ){ .bilinear = true, .has_mips = true, .is_srgb = true, .repeats = true } );
 
   while ( !gfx_should_window_close() ) {
+    double curr_s = gfx_get_time_s();
+
     gfx_poll_events();
 
     int fb_w = 0, fb_h = 0;
@@ -34,14 +39,22 @@ int main( int argc, char** argv ) {
     gfx_clear_colour_and_depth_buffers( 0.2f, 0.2f, 0.2f, 1.0f );
 
     mat4 P = perspective( 67.0f, aspect, 0.1f, 100.0f );
-    mat4 V = look_at( ( vec3 ){ .y = 0.5f, .z = 1.5f }, ( vec3 ){ .y = 0.5f,  }, ( vec3 ){ .y = 1.0f } );
+    mat4 V = look_at( ( vec3 ){ .y = 0.5f, .z = 1.5f },
+      ( vec3 ){
+        .y = 0.5f,
+      },
+      ( vec3 ){ .y = 1.0f } );
     mat4 M = scale_mat4( ( vec3 ){ scale, scale, scale } );
 
-    gfx_draw_mesh( GFX_PT_TRIANGLES, &shader, P, V, M, mesh.vao, mesh.n_vertices, NULL, 0 ); // TODO add shader texture
+    gfx_alpha_testing( true );
+    gfx_uniform1f( &shader, shader.u_time, (float)curr_s );
+    gfx_draw_mesh( GFX_PT_TRIANGLES, &shader, P, V, M, mesh.vao, mesh.n_vertices, &simplex, 1 ); // TODO add shader texture
+    gfx_alpha_testing( false );
 
     gfx_swap_buffer();
   }
 
+  gfx_delete_texture( &simplex );
   gfx_delete_shader_program( &shader );
   gfx_delete_mesh( &mesh );
   gfx_stop();
--- 108_subnautica_build/shader.frag
+++ 108_subnautica_build/shader.frag
@@ -3,10 +3,31 @@
 in vec3 v_n;
 in vec2 v_st;
 
+uniform sampler2D u_texture_a; // simplex
+uniform float u_time;
+
 out vec4 o_frag_colour;
 
 void main() {
+
+  float t = sin( u_time * 0.25 ) * 0.5 + 0.5;
+
+  vec2 st_scroll = vec2( v_st.s + sin( u_time * 0.02 ), v_st.t + cos( u_time * 0.01 ) );
+  vec4 texel = texture( u_texture_a, st_scroll );
+  vec4 texel_circuit = texture( u_texture_a, v_st * 3.0 );
+
+  float f = texel.r;
+
   vec3 n = normalize( v_n );
 
-  o_frag_colour = vec4( v_n, 1.0 );
+  vec3 base_rgb = vec3(0.5,0.5,0.5);
+  
+  float a = 1.0;
+  vec3 rgb = base_rgb;
+  if ( t > f ) {
+    rgb = texel_circuit.r * vec3(0.25,0.25,1.0);
+    a = 0.5;
+  }
+
+  o_frag_colour = vec4( rgb,1.0 );
 }
